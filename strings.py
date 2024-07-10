e4ec1d4ea9c6df3be33c2351b288b2bf4bd44390
Svetlan-37 <karassvetlana@yandex.ru>
2024-07-10 21:47:12 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,12 @@
+name1 = 'Топинамбур'  # пример 1
+print(name1[0])
+print(name1[-1])
+print(name1[5:])
+print(name1[::-1])
+print(name1[1:10:2])
+name2 = 'Эквилибрист'  # пример 2
+print(name2[0])
+print(name2[-1])
+print(name2[6:])
+print(name2[::-1])
+print(name2[1:11:2])
